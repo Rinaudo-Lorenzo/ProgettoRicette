ff44f1c4ba48ce112010bdf68b1e343c24fb6294
RINAUDO LORENZO <l.rinaudo.2301@vallauri.edu>
2023-11-24 20:09:08 +0100

    layout dettagliricette, model ingredienti, font

---
+++ src/app/model/ingrediente.ts
@@ -0,0 +1,9 @@
+export class Ingrediente{
+    public nome: string;
+    public quantita: number;
+
+    constructor(nome:string, quantita:number){
+        this.nome = nome;
+        this.quantita = quantita;
+    }
+}
--- src/app/model/ricette.ts
+++ src/app/model/ricette.ts
@@ -1,14 +1,16 @@
+import { Ingrediente } from "./ingrediente";
+
 export class Ricette{
     public id: string;
     public nome: string;
     public descrizione: string;
-    public ingredienti: string[];
+    public ingredienti: Ingrediente [];
     public preparazione: string[];
     public tempoTotale: number;
     public difficolta: string;
     public URLimmagine: string;
 
-   constructor(id:string, nome:string, descrizione: string, ingredienti:string[], preparazione:string[], tempoTotale:number, difficolta: string, URLimmagine:string){
+   constructor(id:string, nome:string, descrizione: string, ingredienti:Ingrediente[], preparazione:string[], tempoTotale:number, difficolta: string, URLimmagine:string){
         this.id = id;
         this.nome = nome;
         this.descrizione = descrizione;
